9287a589f56683fdf1ba1dfe78e5d08621a02019
Sirjay2018 <anthonyechude2018@gmail.com>
2023-04-11 17:14:36 -0700

    task 1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,31 +16,31 @@
 		
  *      */
 		
-int count_word(char *s)
+	int count_word(char *s)
 		
 {
 		
-		int flag, c, w;
+	int flag, c, w;
 		
 
 		
-			flag = 0;
+	flag = 0;
 		
-				w = 0;
+	w = 0;
 		
 
 		
-					for (c = 0; s[c] != '\0'; c++)
+	for (c = 0; s[c] != '\0'; c++)
 		
-							{
+	{
 		
-										if (s[c] == ' ')
+		if (s[c] == ' ')
 		
-														flag = 0;
+		flag = 0;
 		
-												else if (flag == 0)
+		else if (flag == 0)
 		
-															{
+		{
 		
 																			flag = 1;
 		
@@ -70,7 +70,7 @@ int count_word(char *s)
 		
  */
 		
-char **strtow(char *str)
+	char **strtow(char *str)
 		
 {
 		
